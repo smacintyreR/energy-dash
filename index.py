39c29242e983c8b858f80caf79d2225b11ef1129
Sam MacIntyre <sam.macintyre@barcelonagse.eu>
2021-12-09 21:08:29 +0000

    Basic app created, basic table and graph

--- index.py
+++ index.py
@@ -1,12 +1,27 @@
 from app import app
 from app import server
+import pandas as pd
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_table
 
-app.layout = html.Div([html.H1('Dash Demo Graph',
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
+
+app.layout = html.Div([html.H1('Dash Demo Graph - Sam',
                                style={
                                       'textAlign': 'center',
-                                      "background": "yellow"}),
+                                      "background": "red"}),
+
+
+
+                dash_table.DataTable(
+                            id='table',
+                            columns=[{"name": i, "id": i} for i in df.columns],
+                            data=df.to_dict('records'),
+                                                        ),
+
+
+
                        dcc.Graph(
                            id='graph-1',
                            figure={
@@ -15,7 +30,7 @@ app.layout = html.Div([html.H1('Dash Demo Graph',
                                    {'x': [1, 2, 3, 4, 5, 6, 7], 'y': [12, 22, 36, 44, 49, 58, 73], 'type': 'line', 'name': 'value2'}
                                ],
                                'layout': {
-                                   'title': 'Simple Line Graph',
+                                   'title': 'Line Graph',
                                      }
                                  }
                             ),
